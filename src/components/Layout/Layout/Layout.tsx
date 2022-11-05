8846985ddce30acd9a162d0eef34dac58f14aa9e
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-05 16:18:03 +0900

    NavBarを追加

--- src/components/Layout/Layout/Layout.tsx
+++ src/components/Layout/Layout/Layout.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { NavBar } from "../../parts/NavBar";
 import styles from "./Layout.module.scss";
 export type LayoutProps = {
   children: React.ReactNode;
 };
 export const Layout = (props: LayoutProps) => {
-  return <div className="min-h-screen">{props.children}</div>;
+  return (
+    <div className="min-h-screen">
+      <NavBar />
+      <main>{props.children}</main>
+    </div>
+  );
 };
--- src/components/parts/NavBar/NavBar.tsx
+++ src/components/parts/NavBar/NavBar.tsx
@@ -1,5 +1,39 @@
 import styles from "./NavBar.module.scss";
+import logo from "../../../../public/icon/Logo 1.svg";
+import Image from "next/image";
 
 export const NavBar = () => {
-  return <nav></nav>;
+  const NavBarLinks: Array<{
+    text: string;
+    link: string;
+  }> = [
+    {
+      text: "ABOUT",
+      link: "",
+    },
+    {
+      text: "ACTIVITY",
+      link: "",
+    },
+    {
+      text: "SOCIAL",
+      link: "",
+    },
+    {
+      text: "CONTACT",
+      link: "",
+    },
+  ];
+  return (
+    <nav className="absolute top-0 left-0 z-50 flex h-[210px] w-full p-12">
+      <Image alt="logo image" width={178} height={129} src={logo} />
+      <div className="flex flex-1 flex-row-reverse items-center gap-12">
+        {NavBarLinks.map((link, index) => (
+          <a key={index} href={link.link}>
+            {link.text}
+          </a>
+        ))}
+      </div>
+    </nav>
+  );
 };
