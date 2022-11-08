a8027c6f1142133dde81c6a73c29e27ebc2c36b3
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-08 13:53:52 +0900

    いい感じに開業するようにしsた

--- src/components/parts/NavBar/NavBar.tsx
+++ src/components/parts/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import styles from "./NavBar.module.scss";
 import logo from "../../../../public/icon/Logo 1.svg";
+import { HTMLMotionProps, motion } from "framer-motion";
 import Image from "next/image";
 
 export const NavBar = () => {
@@ -24,16 +25,26 @@ export const NavBar = () => {
       link: "",
     },
   ];
+  const NavBarAnimation: HTMLMotionProps<"nav"> = {
+    initial: { opacity: 0, y: -10 },
+    animate: { opacity: 1, y: 0 },
+    transition: {
+      duration: 0.5,
+    },
+    exit: { opacity: 0, y: 0 },
+  };
   return (
-    <nav className="absolute top-0 left-0 z-50 flex h-[210px] w-full p-12">
-      <Image alt="logo image" width={178} height={129} src={logo} />
-      <div className="flex flex-1 flex-row-reverse items-center gap-12">
-        {NavBarLinks.map((link, index) => (
-          <a key={index} href={link.link}>
-            {link.text}
-          </a>
-        ))}
-      </div>
+    <nav className="absolute top-0 left-0 z-50 m-auto  h-[210px] w-full  p-12">
+      <motion.div {...NavBarAnimation} className="m-auto flex max-w-max">
+        <Image alt="logo image" width={178} height={129} src={logo} />
+        <div className="flex flex-1 flex-row-reverse items-center gap-12">
+          {NavBarLinks.map((link, index) => (
+            <a key={index} href={link.link}>
+              {link.text}
+            </a>
+          ))}
+        </div>
+      </motion.div>
     </nav>
   );
 };
