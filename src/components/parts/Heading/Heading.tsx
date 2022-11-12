182d8b68e66db0f48aa4d57ced012e98b6feeb60
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-12 09:53:30 +0900

    Animationを作ってる

--- src/components/parts/Heading/Heading.tsx
+++ src/components/parts/Heading/Heading.tsx
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect, useRef } from "react";
 import styles from "./Heading.module.scss";
 import { motion } from "framer-motion";
-import gsap from "gsap";
+import { gsap } from "gsap";
 
 type HeadingProps = {
   level: 1 | 2 | 3 | 4 | 5 | 6;
@@ -10,26 +10,41 @@ type HeadingProps = {
 };
 
 export const Heading = (props: HeadingProps) => {
-  const HeadingRef = useRef<HTMLDivElement>(null);
+  const comp = useRef<HTMLDivElement>(null); // create a ref for the root level element (for scoping)
+
   useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      // Target the two specific elements we have asigned the animate class
-      gsap.from;
-    }, HeadingRef); // <- Scope!
+    let ctx = gsap.context(() => {
+      gsap
+        .timeline()
+        .from(".word", {
+          y: 56,
+          opacity: 0,
+          duration: 0.75,
+          ease: "power4.out",
+        })
+        .to(".word", {
+          y: 0,
+          opacity: 1,
+        })
+        .to(".rect", { x: "-120%" });
+    }, comp);
 
     return () => ctx.revert();
-  }, []);
+  });
 
   return (
-    <div ref={HeadingRef}>
+    <div ref={comp}>
+      <span
+        style={{ opacity: 0, transform: "translate( 0 , 100px)" }}
+        className="word  relative inline-block overflow-hidden text-6xl font-black leading-none"
+      >
+        <span className="rect absolute  left-0 h-full w-full bg-black"></span>
+        <span className="label">INTERACTION</span>
+      </span>
+
       <h1
         className={`text-8xl font-bold not-italic text-text ${props.className} gsap-animate`}
       >
-        <span className="word relative inline-block overflow-hidden text-6xl font-black leading-none">
-          <span className="rect absolute top-0 left-0 h-full w-full bg-black"></span>
-          <span className="label">INTERACTION</span>
-        </span>
-
         {props.children}
       </h1>
     </div>
