19fb0df15962790a9bb6db778ef0a97c10db3b86
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-15 13:57:55 +0900

    フォントを  next fontにした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,35 @@
 import type { AppPropsWithLayout } from "next/app";
 
 import { AnimatePresence } from "framer-motion";
+import { Zen_Kaku_Gothic_Antique, Inter } from "@next/font/google";
 
 import "../styles/global.css";
 import "react-modern-drawer/dist/index.css";
+const InterFontNomal = Inter({
+  weight: "400",
+  subsets: ["cyrillic"],
+});
+const InterFontBold = Inter({
+  weight: "700",
+  subsets: ["cyrillic"],
+});
+
+const InterFontVeryBold = Inter({
+  weight: "800",
+  subsets: ["cyrillic"],
+});
+const InterFontThin = Inter({
+  weight: "100",
+  subsets: ["cyrillic"],
+});
+const ZenKakuGothicAntique_normal = Zen_Kaku_Gothic_Antique({
+  weight: "400",
+  subsets: ["japanese"],
+});
+const ZenKakuGothicAntique_bold = Zen_Kaku_Gothic_Antique({
+  weight: "700",
+  subsets: ["japanese"],
+});
 
 export default function MyApp({
   Component,
@@ -14,8 +40,18 @@ export default function MyApp({
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <AnimatePresence mode="wait">
-      {getLayout(<Component key={router.asPath} {...pageProps} />)}
-    </AnimatePresence>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${InterFontBold} ${InterFontNomal} ${InterFontVeryBold}
+              ${InterFontThin} ${ZenKakuGothicAntique_normal.style.fontFamily},
+            ${ZenKakuGothicAntique_bold};
+        }
+      `}</style>
+
+      <AnimatePresence mode="wait">
+        {getLayout(<Component key={router.asPath} {...pageProps} />)}
+      </AnimatePresence>
+    </>
   );
 }
