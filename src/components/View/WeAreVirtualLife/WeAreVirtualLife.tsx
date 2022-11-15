f9610065a5ff413a4b2e6345ce62f7f8f28f7cba
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-15 11:27:19 +0900

    create component

---
+++ src/components/View/WeAreVirtualLife/WeAreVirtualLife.tsx
@@ -0,0 +1,10 @@
+import { BoxAnimation } from "../../features/BoxAnimation";
+import styles from "./WeAreVirtualLife.module.scss";
+
+export const WeAreVirtualLife = () => {
+  return (
+    <BoxAnimation>
+      <div></div>
+    </BoxAnimation>
+  );
+};
