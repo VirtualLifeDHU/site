4f750cf5361fdc463dd68361d053e3fb85aa8898
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-19 12:33:23 +0900

    we are VirtualLifeを入れるコンポーネントを作成

--- src/components/View/WeAreVirtualLife/WeAreVirtualLife.tsx
+++ src/components/View/WeAreVirtualLife/WeAreVirtualLife.tsx
@@ -1,10 +1,38 @@
+import Image from "next/image";
 import { BoxAnimation } from "../../features/BoxAnimation";
+import { Body } from "../../parts/Body";
+import { Heading } from "../../parts/Heading";
 import styles from "./WeAreVirtualLife.module.scss";
 
 export const WeAreVirtualLife = () => {
+  const img = "/imgs/fff.png";
   return (
     <BoxAnimation>
-      <div></div>
+      <div className="relative m-auto ml-auto  max-w-max">
+        <div>
+          <Image
+            className="ml-[auto] max-w-xl"
+            alt="image"
+            src={img}
+            width={1200}
+            height={800}
+          />
+        </div>
+        <div className="absolute top-0 left-0 flex h-full items-center">
+          <div>
+            <Heading level={1} className={"bg-white/80"}>
+              <span>We are </span>
+              <span className={styles.gradesionColor}>Virtual Life</span>
+            </Heading>
+            <Body className="max-w-[calc(100%-600px)]">
+              DHU公認サークル『バーチャルライフ』
+              <br />
+              <br />
+              バーチャルライフはゆるく楽しい創作をしていく事を理念とし、VRに関する創作活動等を行い、人々をバーチャルにつなげていく活動をしています。
+            </Body>
+          </div>
+        </div>
+      </div>
     </BoxAnimation>
   );
 };
