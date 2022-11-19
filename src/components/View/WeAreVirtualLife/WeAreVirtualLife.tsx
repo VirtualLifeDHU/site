5425bf60e364ba751743c6a403b1d13620d8d1b4
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-19 12:41:07 +0900

    透明度を変更

--- src/components/View/WeAreVirtualLife/WeAreVirtualLife.tsx
+++ src/components/View/WeAreVirtualLife/WeAreVirtualLife.tsx
@@ -20,7 +20,7 @@ export const WeAreVirtualLife = () => {
         </div>
         <div className="absolute top-0 left-0 flex h-full items-center">
           <div>
-            <Heading level={1} className={"bg-white/80"}>
+            <Heading level={1} className={"bg-white/90"}>
               <span>We are </span>
               <span className={styles.gradesionColor}>Virtual Life</span>
             </Heading>
