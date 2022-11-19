659f612e4aeafb701241d7fb7cde2c271768da53
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-19 12:33:35 +0900

    フォントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 import { NextPage, NextPageWithLayout } from "next";
 import React from "react";
 import Image from "next/image";
-import { deflate } from "zlib";
 import { Hero } from "../components/parts/Hero";
 
 import styles from "./../styles/index.module.css";
 import { Layout } from "../components/Layout/Layout";
-import { LoadingAnimationA } from "../components/parts/LoadingAnimationA";
 import { About } from "../components/parts/About";
+import { WeAreVirtualLife } from "../components/View/WeAreVirtualLife";
 
 const Home: NextPageWithLayout = () => (
-  <>
+  <div className="flex flex-col gap-32">
     <Hero />
     <About />
-  </>
+    <WeAreVirtualLife />
+  </div>
 );
 
 Home.getLayout = (page) => <Layout>{page}</Layout>;
