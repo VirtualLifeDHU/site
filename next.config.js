10c80696575d10d80a742186a19e69b56f89fa08
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-30 12:48:47 +0900

    add webpack analazer

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 const withExportImages = require("next-export-optimize-images");
-/** @type {import('next').NextConfig} */
-const nextConfig = withExportImages({
-  experimental: {
-    appDir: false,
-  },
-  images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 768, 1024, 1280, 1440, 1980, 2560],
-  },
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withBundleAnalyzer(
+  withExportImages({
+    experimental: {
+      appDir: false,
+    },
+    images: {
+      formats: ["image/avif", "image/webp"],
+      deviceSizes: [640, 768, 1024, 1280, 1440, 1980, 2560],
+    },
+  })
+);
 
 module.exports = nextConfig;
