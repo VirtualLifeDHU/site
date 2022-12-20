887a85eedb894c4ff9a62738fce6925e5385df3d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-20 12:43:26 +0900

    cloudflarepages で動くように変更

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
-const withExportImages = require("next-export-optimize-images");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer(
-  withExportImages({
-    experimental: {
-      appDir: false,
-      scrollRestoration: true,
-    },
-    images: {
-      formats: ["image/avif", "image/webp"],
-      deviceSizes: [640, 768, 1024, 1280, 1440, 1980, 2560],
-    },
-  })
-);
+const nextConfig = withBundleAnalyzer({
+  experimental: {
+    appDir: false,
+    scrollRestoration: true,
+    runtime: "experimental-edge",
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 768, 1024, 1280, 1440, 1980, 2560],
+  },
+});
 
 module.exports = nextConfig;
