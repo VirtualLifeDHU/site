41b78a5992d3d120ff4ade8a846ebb6a0fd3329b
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-01-19 10:02:26 +0900

    test

---
+++ src/components/parts/Join/Join.tsx
@@ -0,0 +1,5 @@
+type JoinProps = {};
+
+export const Join = (props: JoinProps) => {
+  return <></>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,13 @@ import { Hero } from "../components/parts/Hero";
 import { Layout } from "../components/Layout/Layout";
 import { About } from "../components/parts/About";
 import { WeAreVirtualLife } from "../components/View/WeAreVirtualLife";
+import Head from "next/head";
 
 const Home: NextPageWithLayout = () => (
   <div className="flex flex-col gap-32">
+    <Head>
+      <title>VirtualLife</title>
+    </Head>
     <Hero />
     <About />
     <WeAreVirtualLife />
