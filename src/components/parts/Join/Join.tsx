4c19984a6d1553efea2bb6979b15fb18f5f1443b
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-04-17 14:27:14 +0900

    add join compornent

--- src/components/parts/Join/Join.tsx
+++ src/components/parts/Join/Join.tsx
@@ -1,18 +1,31 @@
 import { Heading } from "../Heading";
 import styles from "./Join.module.scss";
+import NextImage from "next/image";
 
 export const Join = () => {
   return (
     <div className="relative m-auto ml-auto  max-w-max">
-      <Heading level={1} className={"bg-white/90 font-bold backdrop-blur"}>
-        <span className={styles.gradesionColor}>Join </span>
-        <span>Virtual Life</span>
-      </Heading>
+      {/* <Heading level={1} className={"bg-white/90 font-bold backdrop-blur"}> */}
+      {/*   <span className={styles.gradesionColor}>Join </span> */}
+      {/*   <span>Virtual Life</span> */}
+      {/* </Heading> */}
+
+      <a href="https://jooinvl.mochi33.com">
+        <NextImage
+          alt="Join Virtual Life"
+          src={"/assets/ogp.svg"}
+          width={800}
+          height={600}
+        />
+      </a>
+
       <a
         href="https://jooinvl.mochi33.com"
-        className={`w-full rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700`}
+        // className={` rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700`}
       >
-        JOIN
+        <div className="duration-50  w-full rounded bg-blue-500 py-2 px-4 text-center font-bold text-white hover:bg-blue-700">
+          <p>Join</p>
+        </div>
       </a>
     </div>
   );
